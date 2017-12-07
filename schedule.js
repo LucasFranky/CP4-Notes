fed4bcec78393b2c62b2d05998f3d2db15c9cd04
LucasFranky <Lucas.frankham19@alumni.loyola.ca>
2017-12-07 14:01:56 -0500

    Add files via upload

---
+++ schedule.js
@@ -0,0 +1,7 @@
+let george = 16;
+let john = ('This is what happens when george is multiplied by 1239');
+console.log(george);
+george = george * 1239;
+console.log(george);
+console.log(john);
+george = george - 21000
