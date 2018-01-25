1421918daca3a6269d2e28c947542fb8f23a7960
LucasFranky <Lucas.frankham19@alumni.loyola.ca>
2018-01-25 10:23:43 -0500

    Add files via upload

--- schedule.js
+++ schedule.js
@@ -5,3 +5,45 @@ george = george * 1239;
 console.log(george);
 console.log(john);
 george = george - 21000
+
+		var expr = 'Calculus';
+switch (expr) {
+  case 'Math.':
+    console.log('Math is hard.');
+    break;
+  case 'English':
+    console.log('Go reading!');
+    break;
+  case 'History':
+    console.log('Thats old news.');
+    break;
+  case 'Science':
+	console.log('Lets learn about space.')
+	break;
+  case 'Th Arts':
+	console.log('Wanna become an actor?')
+	break;
+  case 'CompProg':
+	console.log('Wanna do what I just did?')
+	break;
+  case 'French':
+	console.log('Parle tu le francais?')
+	break;
+  case 'PhysEd':
+	console.log('Lets do some exercise.')
+	break;
+  case 'Speech':
+	console.log('Lets get rid of stage fright together.')
+	break;
+  case 'Homeroom':
+	console.log('Bake sale or movie?')
+	break;
+  case 'Advisors':
+	console.log('Heres some advice.')
+	break;
+  case 'L.A.':
+	console.log('Grade level meeting.')
+	break;
+  default:
+    console.log('Sorry, ' + expr + ' is not a course I am in.');
+}
